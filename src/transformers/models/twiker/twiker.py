aa3aa6ca1d1cecf944943989d1b3bb7b7793a6ad
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-04-16 19:00:51 +0100

    Update twiker.py

--- src/transformers/models/twiker/twiker.py
+++ src/transformers/models/twiker/twiker.py
@@ -47,9 +47,9 @@ class TwikerModel(nn.Module):
         # prepare casual mask
         p = self.kernel_size // 2
         self.casual_mask = torch.ones(p + 1, self.kernel_size)
-        for i in range(1, p + 1):
-            self.casual_mask[i, :i].zero_()  # 11111 => 01111
-            self.casual_mask[i, -i:].zero_()  # 01111 => 01110
+        for i_mask in range(1, p + 1):
+            self.casual_mask[i_mask, :i_mask].zero_()  # 11111 => 01111
+            self.casual_mask[i_mask, -i_mask:].zero_()  # 01111 => 01110
 
     def get_kernel(self, input_ids: torch.LongTensor):
         kernel = self.embedding(input_ids)
@@ -156,99 +156,3 @@ class TwikerModel(nn.Module):
             diff = torch.nn.functional.pad(diff, pad=(0, 0, offset, 0))
             attn_output = attn_output + diff
         return attn_output
-
-
-if __name__ == "__main__":
-    torch.manual_seed(0)
-    # data
-    V, B, H, N, F, K = 7, 8, 11, 9, 13, 5
-    ker_uni_k = torch.randn(K)
-    ker_uni_v = torch.randn(K)
-    k = torch.randn(B, H, N, F)
-    v = torch.randn(B, H, N, F)
-
-    # by TwikerModel
-    ker_k = ker_uni_k.unsqueeze(0).unsqueeze(0).unsqueeze(0).expand(B, N, H, K)
-    ker_v = ker_uni_v.unsqueeze(0).unsqueeze(0).unsqueeze(0).expand(B, N, H, K)
-    ker = torch.cat((ker_k, ker_v), dim=2)
-    emb = TwikerModel(V, K, 1, 1)
-    k1, v1, _, _ = emb.conv_key_value(k, v, ker, for_casual=False)
-
-    # by conv
-    ker_kc = ker_uni_k.unsqueeze(0).unsqueeze(0).unsqueeze(-1).expand(H, 1, K, 1)
-    ker_vc = ker_uni_v.unsqueeze(0).unsqueeze(0).unsqueeze(-1).expand(H, 1, K, 1)
-    k2 = torch.nn.functional.conv2d(k, ker_kc, padding='same', groups=H)
-    v2 = torch.nn.functional.conv2d(v, ker_vc, padding='same', groups=H)
-    print((k1 - k2).abs().max())
-    print((v1 - v2).abs().max())
-
-    # by TwikerModel
-    k1, v1, ck, cv = emb.conv_key_value(k, v, ker, for_casual=True)
-    print((k1 - k2).abs().max())
-    print((v1 - v2).abs().max())
-
-    # masked
-    ker_uni_k[-1:] = 0
-    ker_uni_v[-1:] = 0
-    ker_kc = ker_uni_k.unsqueeze(0).unsqueeze(0).unsqueeze(-1).expand(H, 1, K, 1)
-    ker_vc = ker_uni_v.unsqueeze(0).unsqueeze(0).unsqueeze(-1).expand(H, 1, K, 1)
-    k3 = torch.nn.functional.conv2d(k, ker_kc, padding='same', groups=H)
-    v3 = torch.nn.functional.conv2d(v, ker_vc, padding='same', groups=H)
-    print((ck[..., 0] - k3).abs().max())
-    print((cv[..., 0] - v3).abs().max())
-
-    ker_uni_k[-2:] = 0
-    ker_uni_v[-2:] = 0
-    ker_kc = ker_uni_k.unsqueeze(0).unsqueeze(0).unsqueeze(-1).expand(H, 1, K, 1)
-    ker_vc = ker_uni_v.unsqueeze(0).unsqueeze(0).unsqueeze(-1).expand(H, 1, K, 1)
-    k3 = torch.nn.functional.conv2d(k, ker_kc, padding='same', groups=H)
-    v3 = torch.nn.functional.conv2d(v, ker_vc, padding='same', groups=H)
-    print((ck[..., 1] - k3).abs().max())
-    print((cv[..., 1] - v3).abs().max())
-    print("************")
-
-    # correct attn_weights
-    query_ = torch.randn(B, H, N, F)
-    past = 2
-    key_ = torch.cat((torch.randn(B, H, past, F), k1), dim=2)
-    value_ = torch.cat((torch.randn(B, H, past, F), v1), dim=2)
-    att_w = torch.matmul(query_, key_.transpose(-1, -2))
-
-    # manual correction
-    att_w_manual = att_w.clone()
-    pp = K // 2
-    for i_query in range(N):
-        for i_dist, i_key in enumerate(range(i_query + 1 - pp, i_query + 1)):
-            if i_key < 0:
-                continue
-            q = query_[:, :, i_query:i_query + 1, :]
-            k = ck[:, :, i_key:i_key + 1, :, i_dist]
-            att_w_manual[:, :, i_query, past + i_key] = torch.matmul(q, k.transpose(-1, -2)).squeeze()
-
-    # fast correction
-    att_w_fast = emb.correct_attn_weights_near_casual_boundary(att_w, query_, key_, ck)
-    print((att_w_manual - att_w_fast).abs().max())
-
-    # mask
-    for b in range(B):
-        for h in range(H):
-            att_w_fast[b, h, :, past:] *= torch.ones(N, N).tril()
-
-    # dot with value
-    att_o = torch.matmul(att_w_fast, value_)
-
-    # manual correction
-    att_o_manual = att_o.clone()
-    for i_query in range(N):
-        for i_dist, i_value in enumerate(range(i_query + 1 - pp, i_query + 1)):
-            if i_value < 0:
-                continue
-            value_right = cv[:, :, i_value, :, i_dist]
-            value_wrong = value_[:, :, past + i_value, :]
-            value_diff = value_right - value_wrong
-            w = att_w_fast[:, :, i_query, past + i_value]
-            att_o_manual[:, :, i_query, :] += w[:, :, None] * value_diff
-
-    # fast correction
-    att_o_fast = emb.correct_attn_output_near_casual_boundary(att_o, att_w_fast, value_, cv)
-    print((att_o_manual - att_o_fast).abs().max())
